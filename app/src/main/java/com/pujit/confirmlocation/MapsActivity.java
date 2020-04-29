fd0850a112198d7ae78d21812d48f42a7e7fcd3c
PujitSomaiya <37361593+PujitSomaiya@users.noreply.github.com>
2020-04-29 13:11:18 +0530

    -- markr comming addresss is showing too(issue is showing marker every where when location change)

---
+++ app/src/main/java/com/pujit/confirmlocation/MapsActivity.java
@@ -0,0 +1,299 @@
+package com.pujit.confirmlocation;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private GoogleMap mMap;
+    private GoogleMap.OnCameraIdleListener onCameraIdleListener;
+    private Marker mCurrLocationMarker;
+    private LocationRequest mLocationRequest;
+    private Geocoder geocoder;
+    private List<Address> addresses;
+    private TextView tvCurrentLocation;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        tvCurrentLocation = findViewById(R.id.tvCurrentLocation);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+        configureCameraIdle();
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        if (mGoogleApiClient == null) {
+                            buildGoogleApiClient();
+                        }
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(this, "permission denied",
+                            Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        //Initialize Google Play Services
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient();
+                mMap.setMyLocationEnabled(true);
+                mMap.setOnCameraIdleListener(onCameraIdleListener);
+                getAddress();
+            }
+        } else {
+            buildGoogleApiClient();
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getAddress() {
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            boolean network_enabled = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            Location location;
+
+            if (network_enabled) {
+                location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                if(location!=null){
+
+                    double longitude = location.getLongitude();
+                    double latitude = location.getLatitude();
+                    addresses=geocoder.getFromLocation(latitude,longitude,1);
+                    String address=addresses.get(0).getAddressLine(0);
+                    tvCurrentLocation.setText(address);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                    mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+//        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        LocationManager locationManager = (LocationManager)
+//                getSystemService(Context.LOCATION_SERVICE);
+//        String provider = locationManager.getBestProvider(new Criteria(), true);
+//        if (ActivityCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        Location locations = locationManager.getLastKnownLocation(provider);
+//        List<String> providerList = locationManager.getAllProviders();
+//        if (null != locations && null != providerList && providerList.size() > 0) {
+//            double longitude = locations.getLongitude();
+//            double latitude = locations.getLatitude();
+//            Geocoder geocoder = new Geocoder(getApplicationContext(),
+//                    Locale.getDefault());
+//            try {
+//                List<Address> listAddresses = geocoder.getFromLocation(latitude,
+//                        longitude, 1);
+//                if (null != listAddresses && listAddresses.size() > 0) {
+//                    String state = listAddresses.get(0).getAdminArea();
+//                    String country = listAddresses.get(0).getCountryName();
+//                    String subLocality = listAddresses.get(0).getSubLocality();
+//
+//                    String address=addresses.get(0).getAddressLine(0);
+//                    String locality=addresses.get(0).getLocality();
+//                    tvCurrentLocation.setText(address);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        if (mGoogleApiClient != null) {
+//            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,
+//                    this);
+//        }
+//
+
+    }
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+    private void configureCameraIdle() {
+        onCameraIdleListener = new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker.remove();
+                }
+                LatLng latLng = mMap.getCameraPosition().target;
+                Geocoder geocoder = new Geocoder(MapsActivity.this);
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    if (addressList != null && addressList.size() > 0) {
+                        String state = addressList.get(0).getAdminArea();
+                        String country = addressList.get(0).getCountryName();
+                        String subLocality = addressList.get(0).getSubLocality();
+                        markerOptions.title("" + latLng + "," + subLocality + "," + state
+                                + "," + country);
+                        String locality = addressList.get(0).getAddressLine(0);
+                        if (!locality.isEmpty() && !country.isEmpty())
+                            tvCurrentLocation.setText(locality + "  " + country);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+                if (mMap.addMarker(markerOptions)!=null){
+                    mCurrLocationMarker = mMap.addMarker(markerOptions);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//                    mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+                }else {
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                }
+
+            }
+
+        };
+    }
+
+}
